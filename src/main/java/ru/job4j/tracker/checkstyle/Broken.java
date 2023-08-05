488607f5648b5eb9810ae6a69de53e1e36336c68
RomanKolyagin <romanderyabin@mail.ru>
2023-08-05 13:58:58 +0800

    6. Правила Checkstyle [#504981 #380575]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    private String name;
+    private String surname;
+    public static final String NEW_VALUE = "";
+
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
