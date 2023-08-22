f62ede9b9ba0da4b3ca8866700bc7c8c4b2f776d
RomanKolyagin <romanderyabin@mail.ru>
2023-08-22 22:26:19 +0800

    2. Перегрузка конструктора [#173890 #358224]

--- src/test/java/ru/job4j/TrackerTest.java
+++ src/test/java/ru/job4j/TrackerTest.java
@@ -1,6 +1,9 @@
 package ru.job4j;
 
 import org.junit.Test;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.Tracker;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TrackerTest {
