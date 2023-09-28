9f35f943b692797e31a9fb562295d4d25a7a48e7
RomanKolyagin <romanderyabin@mail.ru>
2023-09-28 23:15:23 +0800

    12.0. Написать тесты на StartUI. [#785 #380600]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,7 +1,20 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class StartUITest {
+public class StartUITest {
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitProgram()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
 }
