35d7b32643ab394408efc2c8540adc746d6a7d61
RomanKolyagin <romanderyabin@mail.ru>
2023-09-28 23:32:54 +0800

    12.0. Написать тесты на StartUI. [#785 #380600]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,20 @@ public class StartUITest {
         new StartUI().init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
+
+    @Test
+    public void whenReplaceAction() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Replaced item"));
+        String replacedName = "New item name";
+        Input in = new StubInput(
+                new String[] {"0", String.valueOf(item.getId()), replacedName, "1" }
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitProgram()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
+    }
 }
