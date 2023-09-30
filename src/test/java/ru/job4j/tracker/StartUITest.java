fb55fe20df9e5048f17c2e6da0282d3eb94c4b06
RomanKolyagin <romanderyabin@mail.ru>
2023-09-30 23:27:36 +0800

    12.2. Тесты вывода на консоль [#33585 #381249]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -149,4 +149,30 @@ public class StartUITest {
                         + "1. Exit program" + ln
         );
     }
+
+    @Test
+    public void whenFindByIdActionTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test"));
+        Input in = new StubInput(
+                new String[] {"0", String.valueOf(one.getId()), "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindById(out),
+                new ExitProgram()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                        + "0. Find by Id" + ln
+                        + "1. Exit program" + ln
+                        + "=== Find item by id ===" + ln
+                        + one + ln
+                        + "Menu." + ln
+                        + "0. Find by Id" + ln
+                        + "1. Exit program" + ln
+        );
+    }
 }
