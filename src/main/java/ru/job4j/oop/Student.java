783970bf948368eddb9639c8ef855e26869da8ee
DarkSide <romanderyabin@mail.ru>
2025-01-15 22:52:12 +0800

    1.2. Вызов метода объекта [#166085 #352917]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,14 +1,14 @@
 package ru.job4j.oop;
 
 public class Student {
-    public void music() {
-        System.out.println("I believe I can fly");
+    public void music(String lyrics) {
+        System.out.println("I can sing a song : " + lyrics);
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
+        petya.music("Tra ta ta ta ta");
+        Student Vasya = new Student();
+        Vasya.music("Purum purum");
     }
 }
