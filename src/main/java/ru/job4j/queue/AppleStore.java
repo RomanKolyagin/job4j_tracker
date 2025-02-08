7c87fa614ae806e5bd69d5327e5d34ca634b42ee
DarkSide <romanderyabin@mail.ru>
2025-02-08 17:52:49 +0800

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,34 @@
+package ru.job4j.queue;
+
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        for (int index = 0; index < queue.size(); index++) {
+            if (queue.size() == count) {
+                break;
+            }
+            queue.poll();
+        }
+        return queue.peek().name();
+    }
+
+    public String getFirstUpsetCustomer() {
+        for (int index = 0; index < queue.size(); index++) {
+            if (queue.size() - count == -1) {
+                break;
+            }
+            queue.poll();
+        }
+        return queue.peek().name();
+    }
+}
