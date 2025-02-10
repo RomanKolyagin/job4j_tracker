d644039707b3f910558e43a0826819bcf8b62496
DarkSide <romanderyabin@mail.ru>
2025-02-11 02:28:02 +0800

    1. Паспорт и жители [#212656]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,6 +11,7 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
     @Test
     public void whenTestAddMethodIsFalse() {
         Citizen citizenOne = new Citizen("2f44a", "Petr Arsentev");
