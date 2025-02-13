403380a12a38ac4e24511a9404a2480bdb7e47d9
DarkSide <romanderyabin@mail.ru>
2025-02-13 16:09:01 +0800

    3. Банковские переводы [#10038]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -17,6 +17,13 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+        }
     }
 
     public User findByPassport(String passport) {
