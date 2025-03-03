889bca5fb527680096f787bb465ff4770776e294
DarkSide <romanderyabin@mail.ru>
2025-03-03 19:29:42 +0800

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/collection/TestSortAscByName.java
+++ src/test/java/ru/job4j/collection/TestSortAscByName.java
@@ -3,8 +3,8 @@ package ru.job4j.collection;
 import org.junit.jupiter.api.Test;
 import ru.job4j.tracker.Item;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +15,7 @@ public class TestSortAscByName {
         Item test2 = new Item("Orange");
         Item test3 = new Item("Potato");
         List<Item> items = Arrays.asList(test2, test1, test3);
-        items.sort(new ItemAscByName());
+        Collections.sort(items);
         List<Item> expected = Arrays.asList(test1, test2, test3);
         assertThat(items).isEqualTo(expected);
     }
@@ -26,7 +26,7 @@ public class TestSortAscByName {
         Item test2 = new Item("Orange");
         Item test3 = new Item("Potato");
         List<Item> items = Arrays.asList(test2, test1, test3);
-        items.sort(new ItemDescByName());
+        Collections.sort(items);
         List<Item> expected = Arrays.asList(test3, test2, test1);
         assertThat(items).isEqualTo(expected);
     }
