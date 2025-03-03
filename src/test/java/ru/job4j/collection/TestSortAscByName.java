4eb424c939b1590fd4920a99c869e63002e93ff8
DarkSide <romanderyabin@mail.ru>
2025-03-03 19:33:03 +0800

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/collection/TestSortAscByName.java
+++ src/test/java/ru/job4j/collection/TestSortAscByName.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import ru.job4j.tracker.Item;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
