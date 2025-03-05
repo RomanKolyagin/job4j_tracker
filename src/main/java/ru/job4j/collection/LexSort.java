10aaaafe4291075269529553e64568b4a17ed3d0
DarkSide <romanderyabin@mail.ru>
2025-03-06 04:55:12 +0800

    4. Сортировка номера [#221284 #534858]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -1,6 +1,5 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
@@ -8,19 +7,8 @@ public class LexSort implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] s1 = o1.split("\\.");
         String[] s2 = o2.split("\\.");
-        int minLength = Math.min(s1.length, s2.length);
-        for (int i = 0; i < minLength; i++) {
-            int num1 = Integer.parseInt(s1[i]);
-            int num2 = Integer.parseInt(s2[i]);
-
-            if (num1 < num2) {
-                return -1;
-            } else if (num1 > num2) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-        return Integer.compare(s1.length, s2.length);
+        int num1 = Integer.parseInt(s1[0]);
+        int num2 = Integer.parseInt(s2[0]);
+        return Integer.compare(num1, num2);
     }
 }
