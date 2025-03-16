c955f3787b3a1354a12b34503a467a4130fa6918
DarkSide <romanderyabin@mail.ru>
2025-03-16 11:44:31 +0800

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int max = Math.max(left.length(), right.length());
+    }
+}
