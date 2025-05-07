fccfcb26cbdea190ce9db85fd07b3061b398f8ae
RomanKolaygin <romanderyabin@mail.ru>
2025-05-07 16:37:00 +0800

    07.05.2025

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -6,5 +6,6 @@ public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int max = Math.max(left.length(), right.length());
+        return max;
     }
 }
